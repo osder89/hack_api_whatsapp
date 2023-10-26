78ed6439cea8277b8279a5ad566e300ef062b7fd
Osder89 <104263852+osder89@users.noreply.github.com>
2023-10-26 16:11:17 -0400

    api

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
-const fs = require('fs');
+
+
+require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
-// Obtén el puerto de las variables de entorno o utiliza el puerto 3000 como valor predeterminado
+// El puerto en el que se ejecutará el servidor
 const port = process.env.PORT ;
+// Configura body-parser para analizar solicitudes JSON
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  const dataToSave =  req.headers.host;
-  const filePath = 'data.json';
-  const jsonData = JSON.stringify(dataToSave, null, 2); 
-fs.writeFile(filePath, jsonData, (err) => {
-  if (err) {
-    console.error('Error al guardar el archivo:', err);
-  } else {
-    console.log('JSON guardado con éxito en ' + filePath);
-  }
-});
-  console.log('Respuesta' + dataToSave );
+// Define un punto final (endpoint) para recibir eventos de WhatsApp
+app.post('/webhook', (req, res) => {
+  const eventData = req.body;
+  // Procesa los eventos de WhatsApp aquí
+  console.log('Evento de WhatsApp recibido:', eventData);
+
+  // Envía una respuesta exitosa
+  res.status(200).json({ message: 'Evento recibido con éxito' });
 });
 
-app.get('/descargar', (req, res) => {
-    // Nombre del archivo descargable
-    const fileName = 'data.json';
-  
-    // Ruta del archivo JSON que deseas enviar
-    const filePath = './data.json'; // Asegúrate de que coincida con la ruta del archivo guardado
-  
-    // Configura la cabecera de la respuesta para la descarga
-    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-    res.setHeader('Content-Type', 'application/json');
-  
-    // Envía el archivo JSON como respuesta
-    res.download(filePath, fileName, (err) => {
-      if (err) {
-        console.error('Error al enviar el archivo:', err);
-      } else {
-        console.log('Archivo JSON enviado con éxito');
-      }
-    });
-  });
-
+// Inicia el servidor
 app.listen(port, () => {
-  console.log('La aplicación está escuchando en el puerto ' + port);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
--- what_wehooks.js
+++ what_wehooks.js
@@ -1,30 +1,21 @@
-// configurar_whatsapp.js
-const express = require('express');
 const axios = require('axios');
 
-const app = express();
+const webhookUrl = 'https://whatsapp.toolhero.tech/api/set_webhook';
+const params = {
+  webhook_url: 'https%3A%2F%2Fhackapiwhatsapp-dev-tfma.2.us-1.fl0.io',
+  enable: true,
+  instance_id: '653A6DD8EDBB0',
+  access_token: '65397a44adc05',
+};
 
-
-app.post('/configurar-whatsapp-webhook', (req, res) => {
-  const webhookURL = 'https://whatsapp.toolhero.tech/api/set_webhook';
-  const params = {
-    webhook_url: 'https://fca1-190-186-42-122.ngrok-free.app/whatsapp/configurar-whatsapp-webhook',
-    enable: true,
-    instance_id: '653A6DD8EDBB0',
-    access_token: '65397a44adc05',
-  };
-
-  axios.post(webhookURL, params)
-    .then(response => {
-      console.log('Respuesta del servidor:');
-      console.log(response.data);
-      res.send('Webhook configurado exitosamente.');
-    })
-    .catch(error => {
-      console.error('Error al configurar el webhook:');
-      console.error(error);
-      res.status(500).send('Error al configurar el webhook.');
-    });
-});
-
-module.exports = app;
+axios.post(webhookUrl, params)
+  .then((response) => {
+    if (response.data.success) {
+      console.log('Webhook establecido con éxito.');
+    } else {
+      console.error('Error al establecer el webhook:', response.data.error);
+    }
+  })
+  .catch((error) => {
+    console.error('Error en la solicitud HTTP:', error);
+  });
